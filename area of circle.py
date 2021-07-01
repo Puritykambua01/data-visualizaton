04feed56fa455e3c3af4085b2352658d51db018c
Puritykambua01 <86780732+Puritykambua01@users.noreply.github.com>
2021-07-01 14:52:23 +0300

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 10:39:08 2021
+
+@author: Admin
+"""
+from math import pi
+    
+r = eval(input('Enter radius: '))
+                  
+area = pi*r*r # pi*r**2
+                  
+print(f'area of a circle with radius {r} is {area}' )
